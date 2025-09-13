8206f2b732f1930473767ae5b7d956c4415413b0
Christopher Villamarín <alexpila.314@gmail.com>
2025-09-12 21:23:56 -0500

    feat: Load and apply FiraCode font to the application

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QFontDatabase, QFont
 from gui.main_window import MainWindow
 from gui.views.emitting_stream import EmittingStream
 
@@ -9,6 +9,13 @@ if __name__ == "__main__":
     
     # Set application icon
     app.setWindowIcon(QIcon("gui/assets/pixel-cat.png"))
+
+    # Load custom font
+    font_id = QFontDatabase.addApplicationFont("gui/assets/FiraCode-VariableFont_wght.ttf")
+    if font_id != -1:
+        font_families = QFontDatabase.applicationFontFamilies(font_id)
+        if font_families:
+            app.setFont(QFont(font_families[0]))
 
     window = MainWindow()
     
